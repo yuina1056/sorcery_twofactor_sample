4504fd64750cdc9d92914d7e48e2565bc3cf59a8
yuina1056 <yuina.i.1056@gmail.com>
2019-02-22 19:48:46 +0900

    create usersessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
