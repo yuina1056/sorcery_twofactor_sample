9da5b5fada0c048cb1931f766cf36c25c393e04f
yuina1056 <yuina.i.1056@gmail.com>
2019-02-22 20:19:45 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_22_103543) do
+ActiveRecord::Schema.define(version: 2019_02_22_111824) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2019_02_22_103543) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "persistence_token"
+    t.string "google_secret"
+    t.boolean "first_twofactor_logged_in"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
